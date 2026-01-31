d03538b160a4df2b02db13bffa723e4ec1c3ab8d
SharmaHemant001 <hs9711747@gmail.com>
2026-01-31 11:28:10 +0530

    Fix case-sensitive model imports

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Session = require("../models/Session");
+const Session = require("../models/session");
 const Attendance = require("../models/attendance");
 const getDistance = require("../utils/distance");
 
